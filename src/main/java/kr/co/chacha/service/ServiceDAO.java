3708075dd4ac0854431097e15a76a0424c113f65
soyoung9312 <sso9312@naver.com>
2023-12-18 18:05:07 +0900

    service패키지, 폴더수정

---
+++ src/main/java/kr/co/chacha/service/ServiceDAO.java
@@ -0,0 +1,12 @@
+package kr.co.chacha.service;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ServiceDAO {
+
+	public ServiceDAO() {
+		System.out.println("----ServiceDAO()객체 생성");
+	}//end
+	
+}//end
