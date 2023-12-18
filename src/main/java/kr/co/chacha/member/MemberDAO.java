e987fef049ddefeefd310aea34d5ea4303671359
jungbin <rla-wjdqls@daum.net>
2023-12-18 23:29:25 +0900

    로그인, 회원가입 관련 java 소스 수정 - 정빈

--- src/main/java/kr/co/chacha/member/MemberDAO.java
+++ src/main/java/kr/co/chacha/member/MemberDAO.java
@@ -1,5 +1,7 @@
 package kr.co.chacha.member;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -8,5 +10,17 @@ public class MemberDAO {
 	public MemberDAO() {
 		System.out.println("----MemberDAO()객체 생성");
 	}//end
+	
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }//end
