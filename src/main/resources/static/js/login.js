dd3583ede5ed665625153deeaac34ba3d3a730c5
jungbin <rla-wjdqls@daum.net>
2023-12-19 11:49:41 +0900

    회원가입 관련 페이지 프론트 수정 - 정빈

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -18,3 +18,37 @@ signin.addEventListener("click", () => {
     loginin.classList.add("block");
     loginup.classList.add("none");
 })
+
+// 예시 입력값
+let tel = document.getelemgetElementById("tel");
+let birth = document.getelemgetElementById("birth");
+
+// 정규표현식을 사용하여 변환
+let formattel = inputValue.replace(/(\d{3})(\d{3})(\d{2})/, "$1-$2-$3");
+
+// 화면에 표시
+console.log(formattedValue); // 브라우저 콘솔에 출력
+document.body.innerHTML = `<p>${formattedValue}</p>`; // HTML body에 표시
+
+
+function oninputPhone(target) {
+    target.value = target.value
+        .replace(/[^0-9]/g, '')
+        .replace(/(^02.{0}|^01.{1}|[0-9]{3,4})([0-9]{3,4})([0-9]{4})/g, "$1-$2-$3");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
