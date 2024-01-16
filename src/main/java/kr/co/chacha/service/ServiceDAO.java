ccf3184255dcda02f8c5281816452cea1dc4a45b
soyoung9312 <sso9312@naver.com>
2024-01-16 16:02:04 +0900

    service 모집인원체크 완료

--- src/main/java/kr/co/chacha/service/ServiceDAO.java
+++ src/main/java/kr/co/chacha/service/ServiceDAO.java
@@ -24,53 +24,57 @@ public class ServiceDAO {
 
 	public ServiceDAO() {
 		System.out.println("----ServiceDAO()객체 생성");
-	}//end
-	
+	}// end
+
 	@Autowired
 	SqlSession sqlSession;
-	
+
 	public void insert(ServiceDTO servicedto) {
 		sqlSession.insert("service.insert", servicedto);
-	}//insert() end
-	
-	//페이징,검색
+	}// insert() end
+
+	// 페이징,검색
 	public List<ServiceDTO> serviceList(int currentPage, int limit, String type, String keyword) {
-		int offset = (currentPage-1)*limit;
+		int offset = (currentPage - 1) * limit;
 		RowBounds rowBounds = new RowBounds(offset, limit);
-		
+
 		ServiceDTO dto = new ServiceDTO();
-		if(!StringUtils.isEmpty(type) && !StringUtils.isEmpty(keyword)) {
+		if (!StringUtils.isEmpty(type) && !StringUtils.isEmpty(keyword)) {
 			dto.setType(type);
 			dto.setKeyword(keyword);
 		}
 		return sqlSession.selectList("service.serviceList", dto, rowBounds);
 	}
-	
+
 	public int serviceListCnt() {
 		return sqlSession.selectOne("service.serviceListCnt");
 	}
-	
-	public ServiceDTO detail(int sno){
+
+	public ServiceDTO detail(int sno) {
 		return sqlSession.selectOne("service.detail", sno);
 	}
-	
-	public ServiceDTO update(ServiceDTO servicedto){
+
+	public ServiceDTO update(ServiceDTO servicedto) {
 		return sqlSession.selectOne("service.update", servicedto);
 	}
-	
-	public ServiceDTO delete(int sno){
+
+	public ServiceDTO delete(int sno) {
 		return sqlSession.selectOne("service.delete", sno);
 	}
-	
+
 	public void insertServicea(ServiceDTO servicedto) {
 		sqlSession.insert("service.insertServicea", servicedto);
-	}//insert() end
-	
-	//조회수
+	}// insert() end
+
+	// 조회수
 	public void increaseViewCount(int sno) {
-	    // 게시물 조회수 증가 로직 구현
-	    // 예: 데이터베이스에서 해당 sno의 게시물을 찾아 조회수를 1 증가시키는 쿼리 실행
-		 sqlSession.update("service.increaseViewCount", sno);
+		// 게시물 조회수 증가 로직 구현
+		// 예: 데이터베이스에서 해당 sno의 게시물을 찾아 조회수를 1 증가시키는 쿼리 실행
+		sqlSession.update("service.increaseViewCount", sno);
 	}
-	
-}//end
+
+	public int checkPersonCnt(int sno) {
+		return sqlSession.selectOne("service.checkPersonCnt", sno);
+	}
+
+}// end
